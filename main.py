0ad256747c85d05458ec3408c06382c052d348fa
nandu <nandhakumar23102003@gmail.com>
2024-06-26 23:13:51 +0530

    added twin ball

--- main.py
+++ main.py
@@ -21,8 +21,9 @@ POWER_BALL_RADIUS = 17
 LIFE_TIME_POWER_BALL = 9
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-POWER_LIST = ['power', 'blinking_ball', 'large', 'small']
-POWER_COLOR_MAP = {POWER_LIST[0]:PURPLE, POWER_LIST[1]:RED, POWER_LIST[2]:GREEN, POWER_LIST[3]:ORANGE}
+BLUE = (9, 239, 239)
+POWER_LIST = ['power', 'blinking_ball', 'large', 'small', 'twin']
+POWER_COLOR_MAP = {POWER_LIST[0]:PURPLE, POWER_LIST[1]:RED, POWER_LIST[2]:GREEN, POWER_LIST[3]:ORANGE, POWER_LIST[4]:BLUE}
 
 FONT = pygame.font.SysFont("Times New Roman", 30)
 clock = pygame.time.Clock()
@@ -39,6 +40,7 @@ class Paddel:
         self.is_hit_ball = False
         self.velocity = PADDLE_VEL
         self.score = 0
+
 
     def draw(self, win):
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0, 2)
@@ -74,11 +76,10 @@ class Ball:
         else:
             return False
 
-
 def color():
     return (randint(0, 255), randint(0, 255), randint(0, 255))
 
-def draw(win, paddle_1, paddle_2, balls):
+def draw(win, paddle_1, paddle_2, balls, power_balls):
     win.fill("black")
     pygame.draw.line(win, "white", (WIDTH // 2, 0), (WIDTH // 2, HEIGHT), 2)
     pygame.draw.circle(win, "white", (WIDTH // 2, HEIGHT //2), 40, 2)
@@ -104,6 +105,8 @@ def draw(win, paddle_1, paddle_2, balls):
     paddle_2.draw(win)
     for ball in balls:
         ball.draw(win)
+    for power_ball in power_balls:
+        power_ball.draw(win)
     pygame.display.update()
     
 def paddle_movement(keys, paddle_1, paddle_2):
@@ -116,61 +119,73 @@ def paddle_movement(keys, paddle_1, paddle_2):
     if keys[pygame.K_DOWN] and paddle_2.y + paddle_2.height < HEIGHT:
         paddle_2.move_down()
            
-def ball_movement(keys, paddle_1 , paddle_2, ball):
-    ball.move_x()
-    ball.move_y()
+def ball_movement(keys, paddle_1 , paddle_2, balls):
+    for ball in balls:
+        ball.move_x()
+        ball.move_y()
 
-    if ball.y + ball.radius + (ball.y_vel // 2) >= HEIGHT or ball.y - ball.radius <= -ball.y_vel // 2:
-        ball.y_vel = -ball.y_vel
+        if ball.y + ball.radius + (ball.y_vel // 2) >= HEIGHT or ball.y - ball.radius <= -ball.y_vel // 2:
+            ball.y_vel = -ball.y_vel
 
-    if ball.x + ball.radius + (ball.x_vel // 2) >= paddle_2.x and ball.y in range(paddle_2.y, paddle_2.y + paddle_2.height + 1):
-        #for purple power
-        if paddle_2.color == PURPLE and ball.x_vel <= 24:
-            ball.x_vel = int(ball.x_vel * 1.9) 
-        if paddle_2.color != PURPLE and ball.x_vel > 12:
-            ball.x_vel //= 1.3
+        if ball.x + ball.radius + (ball.x_vel // 2) >= paddle_2.x and ball.y in range(paddle_2.y, paddle_2.y + paddle_2.height + 1):
+            #for purple power
+            if paddle_2.color == PURPLE and ball.x_vel <= 24:
+                ball.x_vel = int(ball.x_vel * 1.9) 
+            if paddle_2.color != PURPLE and ball.x_vel > 12:
+                ball.x_vel //= 1.3
 
-        ball.x_vel = -ball.x_vel
-        dis = ball.y - (paddle_2.y + paddle_1.height // 2)
-        if keys[pygame.K_UP]:
-            dis -= 2
-        elif keys[pygame.K_DOWN]:
-            dis += 2
-        ball.y_vel = BALL_MAX_Y_VEL * dis // 40
+            ball.x_vel = -ball.x_vel
+            dis = ball.y - (paddle_2.y + paddle_1.height // 2)
+            if keys[pygame.K_UP]:
+                dis -= 2
+            elif keys[pygame.K_DOWN]:
+                dis += 2
+            ball.y_vel = BALL_MAX_Y_VEL * dis // 40
 
-    elif ball.x - ball.radius + (ball.x_vel // 2) <= paddle_1.x + paddle_1.width and ball.y in range(paddle_1.y, paddle_1.y + paddle_1.height + 1):
-        #for purple power
-        if paddle_1.color == PURPLE and ball.x_vel >= -24:
-            ball.x_vel = int(ball.x_vel * 1.9)
-        if paddle_1.color != PURPLE and ball.x_vel < -12:
-            ball.x_vel //= 1.3
+        elif ball.x - ball.radius + (ball.x_vel // 2) <= paddle_1.x + paddle_1.width and ball.y in range(paddle_1.y, paddle_1.y + paddle_1.height + 1):
+            #for purple power
+            if paddle_1.color == PURPLE and ball.x_vel >= -24:
+                ball.x_vel = int(ball.x_vel * 1.9)
+            if paddle_1.color != PURPLE and ball.x_vel < -12:
+                ball.x_vel //= 1.3
 
-        ball.x_vel = -ball.x_vel
-        dis = ball.y - (paddle_1.y + paddle_1.height // 2)
-        if keys[pygame.K_w]:
-            dis -= 2
-        elif keys[pygame.K_s]:
-            dis += 2
-        ball.y_vel = BALL_MAX_Y_VEL * dis // 40
+            ball.x_vel = -ball.x_vel
+            dis = ball.y - (paddle_1.y + paddle_1.height // 2)
+            if keys[pygame.K_w]:
+                dis -= 2
+            elif keys[pygame.K_s]:
+                dis += 2
+            ball.y_vel = BALL_MAX_Y_VEL * dis // 40
 
-def score_count(paddle_1, paddle_2, ball):
+def score_count(paddle_1, paddle_2, balls):
     hit = False
-    if ball.x + ball.radius >= WIDTH:
-        ball.x = WIDTH // 2
-        ball.y = HEIGHT // 2
-        ball.x_vel = BALL_START_X_VEL
-        ball.y_vel = 0
-        paddle_1.score += 1
-        hit = True
-    if ball.x - ball.radius <= 0:
-        ball.x = WIDTH // 2
-        ball.y = HEIGHT // 2
-        ball.x_vel = BALL_START_X_VEL
-        ball.y_vel = 0
-        paddle_2.score += 1
-        hit = True
+    i = 0
+    while i < len(balls):
+        ball = balls[i]
+        if ball.x + ball.radius >= WIDTH:
+            paddle_1.score += 1
+            if len(balls) == 2:
+                balls.pop(i)
+            else:
+                ball.x = WIDTH // 2
+                ball.y = HEIGHT // 2
+                ball.x_vel = BALL_START_X_VEL
+                ball.y_vel = 0
+                hit = True
+        if ball.x - ball.radius <= 0:
+            paddle_2.score += 1
+            if len(balls) == 2:
+                balls.pop(i)
+            else:
+                ball.x = WIDTH // 2
+                ball.y = HEIGHT // 2
+                ball.x_vel = BALL_START_X_VEL
+                ball.y_vel = 0
+                hit = True
+        i += 1
 
     if hit:   #to reset size after round
+        ball = balls[0]
         paddle_1.y = HEIGHT // 2 - PADDLE_HEIGHT // 2
         paddle_2.y = HEIGHT // 2 - PADDLE_HEIGHT // 2
         paddle_1.height = PADDLE_HEIGHT
@@ -277,51 +292,63 @@ def power_ball_power(paddle_1, paddle_2, power_handeler):
         paddle_2.color = PURPLE
 
     if time.time() - power_handeler[2] > 6:
-        power_handeler[1] == None
+        power_handeler[1] = None
         paddle_1.color = "white"
         paddle_2.color = "white"
-    
-def power_ball_spawn(is_power_ball_present,power_handeler, balls):
+
+def power_ball_spawn(is_power_ball_present,power_handeler, balls, power_balls):
     if not is_power_ball_present:
         if FREQ_OF_POWER < time.time() - power_handeler[0]:
             power_ball_spawned = True
-            x = randint(WIDTH // 9, WIDTH - (WIDTH // 9))
-            y = randint(30 + POWER_BALL_RADIUS, HEIGHT - (30 + POWER_BALL_RADIUS))
             type_of_power = choice(POWER_LIST)
-            power_ball = Ball(x, y, POWER_BALL_RADIUS, POWER_COLOR_MAP[type_of_power])
-            power_ball.x_vel = 4
-            balls.append(power_ball)
+            color_chosen = POWER_COLOR_MAP[type_of_power]
+            if color_chosen == BLUE:
+                x = WIDTH // 2
+                y = HEIGHT // 2
+                ball_x_vel = 0
+                power_ball = Ball(x, y, POWER_BALL_RADIUS + 2, color_chosen)
+                power_ball.y_vel = BALL_MAX_Y_VEL - 3
+            else:
+                x = randint(WIDTH // 9, WIDTH - (WIDTH // 9))
+                y = randint(30 + POWER_BALL_RADIUS, HEIGHT - (30 + POWER_BALL_RADIUS))
+                ball_x_vel = 4
+                power_ball = Ball(x, y, POWER_BALL_RADIUS, color_chosen)
+            power_ball.x_vel = ball_x_vel
+            power_balls.append(power_ball)
             global power_spawn_time
             power_spawn_time = time.time()
             return  power_ball_spawned
     else:
         #for power ball movement
-        balls[1].move_x()
-        balls[1].move_y()
-        if (balls[1].y + balls[1].radius + (balls[1].y_vel // 2) >= HEIGHT) or (balls[1].y - balls[1].radius <= -balls[1].y_vel // 2):
-            balls[1].y_vel *= -1
-        if (balls[1].x + balls[1].radius >= WIDTH) or (balls[1].x - balls[1].radius <= 0):
-            balls[1].x_vel *= -1
+        power_balls[0].move_x()
+        power_balls[0].move_y()
+        if (power_balls[0].y + power_balls[0].radius + (power_balls[0].y_vel // 2) >= HEIGHT) or (power_balls[0].y - power_balls[0].radius <= -power_balls[0].y_vel // 2):
+            power_balls[0].y_vel *= -1
+        if (power_balls[0].x + power_balls[0].radius >= WIDTH) or (power_balls[0].x - power_balls[0].radius <= 0):
+            power_balls[0].x_vel *= -1
 
-        if balls[0].collusion(balls[1]):
-            if balls[0].x_vel > 0:
-                power_handeler[3] = 1
-            else:
-                power_handeler[3] = 2
-            power_handeler[2] = time.time()
-            if balls[-1].color == PURPLE:
-                power_handeler[1] = POWER_LIST[0]
-            elif balls[-1].color == RED:
-                power_handeler[1] = POWER_LIST[1]
-            elif balls[-1].color == GREEN:
-                power_handeler[1] = POWER_LIST[2]
-            else:
-                power_handeler[1] = POWER_LIST[3]  
-            balls.pop()
-            power_handeler[0] = time.time() - 3     #for cool down
-            return False
-        elif  time.time() - power_spawn_time > LIFE_TIME_POWER_BALL:
-            balls.pop()
+        for ball in balls:
+            if ball.collusion(power_balls[0]):
+                if ball.x_vel > 0:
+                    power_handeler[3] = 1
+                else:
+                    power_handeler[3] = 2
+                power_handeler[2] = time.time()
+                if power_balls[-1].color == PURPLE:
+                    power_handeler[1] = POWER_LIST[0]
+                elif power_balls[-1].color == RED:
+                    power_handeler[1] = POWER_LIST[1]
+                elif power_balls[-1].color == GREEN:
+                    power_handeler[1] = POWER_LIST[2]
+                elif power_balls[-1].color == ORANGE:
+                    power_handeler[1] = POWER_LIST[3]
+                else:
+                    power_handeler[1] = POWER_LIST[4]
+                power_balls.pop()
+                power_handeler[0] = time.time() - 3     #for cool down
+                return False
+        if time.time() - power_spawn_time > LIFE_TIME_POWER_BALL:
+            power_balls.pop()
             power_handeler[0] = time.time() - 3
             return False
         return True
@@ -331,6 +358,7 @@ def main():
     paddle_1 = Paddel(10)
     paddle_2 = Paddel(WIDTH - 10 - paddle_1.width)
     balls = []
+    power_balls = []
     ball = Ball(WIDTH // 2, HEIGHT // 2, BALL_RADIUS, "white")
     balls.append(ball)
     #power
@@ -356,9 +384,9 @@ def main():
                 ball.x_vel += 1
             else:
                 ball.x_vel -= 1
-        ball_movement(keys, paddle_1, paddle_2, ball)
-        running = score_count(paddle_1, paddle_2, ball)
-        is_power_ball_present = power_ball_spawn(is_power_ball_present,power_handeler, balls)
+        ball_movement(keys, paddle_1, paddle_2, balls)
+        running = score_count(paddle_1, paddle_2, balls)
+        is_power_ball_present = power_ball_spawn(is_power_ball_present,power_handeler, balls, power_balls)
         if power_handeler[1]:
             if power_handeler[1] == POWER_LIST[0]: #purple power
                 power_ball_power(paddle_1, paddle_2, power_handeler)
@@ -368,7 +396,16 @@ def main():
                 large_paddle_power(paddle_1, paddle_2, power_handeler)
             elif power_handeler[1] == POWER_LIST[3]: #small paddle (orange power)
                 small_paddle_power(paddle_1, paddle_2, power_handeler)
-        draw(WIN, paddle_1, paddle_2, balls)
+            elif power_handeler[1] == POWER_LIST[4]: #twin power
+                if len(balls) < 3:
+                    x = balls[0].x
+                    y = balls[0].y
+                    new_ball =  Ball(x, y, BALL_RADIUS, "white")
+                    new_ball.x_vel = -(balls[0].x_vel)
+                    new_ball.y_vel = balls[0].y_vel
+                    balls.append(new_ball)
+                    power_handeler[1] = None
+        draw(WIN, paddle_1, paddle_2, balls, power_balls)
         count += 1
         if count > 60:
             print(power_handeler)
